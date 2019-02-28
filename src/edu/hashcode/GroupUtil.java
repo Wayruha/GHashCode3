d451ce8f061442a8b8f0404e4bb8b16c4580134d
andrkomarov <andr.komarov@gmail.com>
2019-02-28 22:33:14 +0200

    ololo

---
+++ src/edu/hashcode/GroupUtil.java
@@ -0,0 +1,11 @@
+package edu.hashcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupUtil {
+
+    public static List<Slide> groupInsideTag(Slide left, Slide right) {
+        return new ArrayList<>();
+    }
+}
